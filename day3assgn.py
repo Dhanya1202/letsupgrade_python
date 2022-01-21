085a5e6f3d1cf07537f4038771cb65a783d10146
Dhanya1202 <95705962+Dhanya1202@users.noreply.github.com>
2022-01-21 12:21:31 +0530

    Add files via upload

---
+++ day3assgn.py
@@ -0,0 +1,11 @@
+details=[]
+details.append(input("Enter your first name:"))
+details.append(input("Enter your last name:"))
+details.append(int(input("Enter your age:")))
+details.append(input("Enter your address:"))
+details.append(input("Enter your hobbies:"))
+details.append(input("Enter you date of birth:"))
+print("Details")
+print(details)
+for i in details:
+    print(i)
